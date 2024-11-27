693bbfa21a2457ff9b95fbde6b50f2d8cd00de51
Thiago de Paula <thiago@tenil.com.br>
2024-11-27 17:54:42 -0300

    Cloudflare

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,8 @@ const tailwind = require("tailwindcss")(tailwindConfig);
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  // eslint-disable-next-line no-process-env
   plugins: [
     tailwind,
     ...(process.env.HUGO_ENVIRONMENT === "production" ? [autoprefixer] : []),
-    autoprefixer,
   ],
 };
